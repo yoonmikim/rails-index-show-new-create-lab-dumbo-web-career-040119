6f22343eedea4f0060074d18077e2f2eacf9ec90
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-29 20:10:08 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  resources :coupon, only: [:index, :show, :edit, :update, :create]
 end
