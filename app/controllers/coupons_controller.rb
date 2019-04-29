c70b682ea6d244b6649d58a65aa4632ee86c2600
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-29 20:37:06 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -10,11 +10,11 @@ class CouponsController < ActionController::Base
   end
 
   def new 
-    @coupon = Coupon.new 
+    # @coupon = Coupon.new 
   end
   
   def create
-    @coupon = Coupon.create
+    @coupon = Coupon.new
     @coupon[:coupon_code] = params[:coupon][:coupon_code]
     @coupon[:store] = params[:coupon][:store]
     @coupon.save
