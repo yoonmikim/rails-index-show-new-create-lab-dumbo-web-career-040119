8a7a2a6a5f2ae8f52937a682f1afad37360c6ba9
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-29 20:31:26 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -9,29 +9,36 @@ class CouponsController < ActionController::Base
     @coupon = Coupon.new 
   end
   
-  
-  def create 
-    @coupon = Coupon.create(coupon_params)
-    redirect_to @coupon
+  def create
+    @coupon = Coupon.new
+    @coupon[:coupon_code] = params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
   end
   
-  def edit 
+  # def create 
+  #   @coupon = Coupon.create(coupon_params)
+  #   redirect_to @coupon
+  # end
+  
+  # def edit 
     
-  end
+  # end
   
-  def update 
-    @coupon.update(coupon_params)
-    redirect_to @coupon
-  end
+  # def update 
+  #   @coupon.update(coupon_params)
+  #   redirect_to @coupon
+  # end
   
-  private 
+  # private 
   
-  def coupon_params
-    params.require(:coupon).permit(:coupon_code, :store)
-  end
+  # def coupon_params
+  #   params.require(:coupon).permit(:coupon_code, :store)
+  # end
   
-  def get_coupon
-    @coupon = Coupon.find(params[:id])
-  end
+  # def get_coupon
+  #   @coupon = Coupon.find(params[:id])
+  # end
   
 end
